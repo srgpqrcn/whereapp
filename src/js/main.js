90b7dbfbde744b4788c3c891208628159736ba03
Sergi Piquer Cano <srgpqrcn@gmail.com>
2024-02-18 11:21:07 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
+///USER LOGIN
+
+//User login interaction
+const loginArea = document.getElementById("loginContainer");
+const loginInputName = document.getElementById("inputName");
+const loginInputSession = document.getElementById("inputSession");
+const loginBut = document.getElementById("loginBut");
+let loginName;
+let loginSession;
+
+const userLogin = () => {
+    if(loginInputName.value.length>0 && loginInputSession.value.length>0){
+        loginName = loginInputName.value;
+        loginSession = loginInputSession.value;
+        infoName.textContent=loginName;
+        loginArea.style.display='none';
+        initApp();
+    }else{
+        alert("Please, introduce an user name and session ID.");
+    }
+};
+
+//User interaction - Events
+loginBut.onclick=userLogin;
+
 /////////DB
 
 ///Data base initialization.
@@ -5,12 +30,26 @@ const db = {
     markers: []
 };
 
-
 /////////MAP
 
 ///Map creation
-const mapArea = document.getElementById("map");
-mapArea.style.display = 'none';
+const mapArea = document.getElementById("mapContainer");
+const infoMap = document.getElementById("infoMap");
+const infoName = document.getElementById("infoName");
+const infoConnections = document.getElementById("infoConnections");
+
+const myCircleColor = '#ff0000';
+const usersCircleColor = '#004aff';
+
+const circleOptions ={
+    radius:10,
+    stroke:true,
+    color:'#000000',
+    weight:1.5,
+    fill:true,
+    fillColor:'#000000',
+    fillOpacity:1
+}
 
 const markerGroup = L.featureGroup();
 let mapfollowMode = false; 
@@ -19,7 +58,7 @@ const MAX_ZOOM_AUTO = 19;
 const MIN_ZOOM_AUTO = 1;
 
 let eventId;
-let zoomView=false;
+
  
 const map = L.map('map'); 
 
@@ -41,7 +80,7 @@ const initMap = () =>{
     map.off('dblclick');
 
     //Add event to remove 'move' events from all markers when user clicks map area
-    map.on('click',()=>{
+    map.on('dblclick',()=>{
         db.markers.map((user)=>{
             user.marker.off('move');
             });
@@ -57,14 +96,14 @@ const updateMap = (userData,action) => {
     
     let i;
     let oldUser=false;
-        
+    
     db.markers.map((user,index)=>{
         if(user.name === userData.name){
             oldUser=true;
             i=index;
         }
     });
-        
+    
     if (oldUser){
         if(action==='update'){
            //Update only if position change
@@ -83,35 +122,46 @@ const updateMap = (userData,action) => {
             db.markers.splice(i,1);
             };
         };
-    }else{
+    }else{ 
+        //New User
         //Adding marker to user data object.
-        userData.marker=L.marker(userData.position).bindPopup(userData.name); 
+        //Red color for user marker / blue color for other users.
+        if(userData.name === loginName){
+           circleOptions.fillColor=myCircleColor; 
+        }else{
+            circleOptions.fillColor=usersCircleColor;
+        }
+        userData.marker=L.circleMarker(userData.position,circleOptions);
+        //Delete all default marker events.
+        userData.marker.off(); 
         //Adding user object data to database.
         db.markers.push(userData);
         //Adding marker to map layer.
         map.addLayer(userData.marker); 
         //Add marker to marker group.
         markerGroup.addLayer(userData.marker);
-        
+        //Add click event to marker.
         userData.marker.on('click',(ev)=>{   
             followMarker(ev);
             console.log("click on marker");
         });
-
-    };
-    if (!mapfollowMode){ 
+        if (!mapfollowMode){ 
         fitMarkers()
+        };
     };
+    updateInfoMapData();
 };
 
 //Follow marker position
 const followMarker = (markerEvent) => {
     mapfollowMode = true;
+    console.log("follow mode")
     map.setView(markerEvent.target.getLatLng(),MAX_ZOOM_AUTO);
+    console.log(markerEvent.target.getLatLng());
     markerEvent.target.on('move',(ev)=>{
-        //map.setView(ev.target.getLatLng(),MAX_ZOOM_AUTO);
         map.panTo(ev.target.getLatLng());
         console.log("siguiendo marker");
+        console.log(ev.target.getLatLng());
     })
 };
 
@@ -123,6 +173,9 @@ const fitMarkers = () => {
     
 };
 
+const updateInfoMapData = () => {
+    infoConnections.textContent=db.markers.length-1;
+};
 
 ///Geolocation functions
 
@@ -213,41 +266,4 @@ const initApp = () => {
 const stop = () => {
     clearInterval(eventId); 
     console.log("APP DETENIDA");
-}
-
-///USER LOGIN
-
-//User login interaction
-const loginArea = document.getElementById("loginContainer");
-const loginInputName = document.getElementById("inputName");
-const loginInputSession = document.getElementById("inputSession");
-const loginBut = document.getElementById("loginBut");
-let loginName;
-let loginSession;
-
-/// Testing Password
-const adminSessionID = "admin12345#"
-
-const userLogin = () => {
-    let loginSucces = false;
-    if(loginInputName.value.length>0 && loginInputSession.value.length>0){
-        loginName = loginInputName.value;
-        loginSession = loginInputSession.value;
-        if (loginSession===adminSessionID){
-            loginArea.style.display='none';
-            initApp();
-        }else{
-            alert("Enter a Valid Session ID");
-        }
-        
-       
-
-    }else{
-        alert("Please, introduce an user name and session ID.");
-    }
 };
-
-
-//User interaction - Events
-loginBut.onclick=userLogin;
-
